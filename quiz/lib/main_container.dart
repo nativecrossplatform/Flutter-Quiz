5ecf06761b94428554cce5378101e77f4582f5bc
Jash Upadhyay <jashupadhyay.work@gmail.com>
2025-01-30 17:38:15 +0530

    Home Page Structure Set Up

--- quiz/lib/main_container.dart
+++ quiz/lib/main_container.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
-class MainContainer extends StatelessWidget
-{
-      const MainContainer({super.key});
-      @override
-      Widget build(BuildContext context) 
-      {
-          return Container();
-      }
+import 'package:quiz/Pages/Home.dart';
+class MainContainer extends StatelessWidget {
+  const MainContainer({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: [Colors.deepPurple, Colors.deepPurpleAccent],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter)),
+      child: HomePage(),
+    );
+  }
 }
