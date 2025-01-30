a060f0aec2265e55a36bc324ed9a84aaa734e992
Jash Upadhyay <jashupadhyay.work@gmail.com>
2025-01-30 18:41:16 +0530

    home page complete

--- quiz/lib/Pages/home_page.dart
+++ quiz/lib/Pages/home_page.dart
@@ -1,17 +1,35 @@
 import 'package:flutter/material.dart';
-class HomePage extends StatelessWidget
+
+class HomePage extends StatefulWidget
 {
-      const HomePage({super.key});
-      @override
-      Widget build(BuildContext context) 
-      {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset('./Assets/quiz-logo.png',height: 200,width: 200),
-                const Text('Learn Flutter The Fun Way'),
-                OutlinedButton(onPressed: ()=>{}, child: const Text('Start Quiz'))
-              ],
-            );
-      }
+    const HomePage({super.key});
+    @override
+    State<HomePage> createState() => _HomePage();
+}
+class _HomePage extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset('./Assets/quiz-logo.png', height: 200, width: 200),
+          SizedBox(height: 20),
+          const Text('Learn Flutter The Fun Way',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.normal,
+                  fontStyle: FontStyle.normal,
+                  decoration: TextDecoration.none)),
+          SizedBox(height: 20),
+          OutlinedButton(
+              onPressed: () => {},
+              child: const Text('Start Quiz',
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.normal)))
+        ],
+      ),
+    );
+  }
 }
